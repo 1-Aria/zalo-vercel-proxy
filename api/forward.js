87a914a5866304cdad9966a3666e5a4f92c4c723
1-Aria <dhquangphuong@gmail.com>
2025-10-17 15:01:16 +0700

    test zalo payload

--- api/forward.js
+++ api/forward.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
     // Then handle the forward in the background
     try {
-      await fetch("https://script.google.com/macros/s/AKfycbx123xyz/exec", {
+      await fetch("https://script.google.com/macros/s/AKfycbxTUqUYhz9sNpp1SFTdwS4eK4z6_Rb_I49lU17vPdPiNJM1d9AHKvHYO4y8NgHntN97zA/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(req.body)
